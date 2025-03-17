5b531c8124f62c55a08e80cd741f75d125d196c4
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-03-17 18:11:19 +0900

    no message

--- mini_project03/js/contact_us.js
+++ mini_project03/js/contact_us.js
@@ -1,9 +1,80 @@
 const $boardDiv = document.querySelector('#board-div');
 
+// 게시글 작성 및 등록
+document.querySelector('#submit-post').addEventListener('click', async function () {
+    const title = document.querySelector('#post-title').value;
+    const category_id = document.querySelector('#post-category').value;
+    const author = document.querySelector('#user-name').value;
+    const content = document.querySelector('#post-content').value;
+    const password = document.querySelector('#post-password').value;
+    const image_url = document.querySelector('#post-image-url').files[0];
+
+    if (title.length == 0) {
+        Swal.fire({icon: "error", title: "등록실패", text: "제목을 입력해주세요."})
+            .then(() => {
+                document.querySelector('#post-title').focus();
+            });
+        return;
+    } else if (author.length == 0) {
+        Swal.fire({icon: "error", title: "등록실패", text: "작성자를 입력해주세요."})
+            .then(() => {
+                document.querySelector('#user-name').focus();
+            });
+        return;
+    } else if (content.length == 0) {
+        Swal.fire({icon: "error", title: "등록실패", text: "내용을 입력해주세요."})
+            .then(() => {
+                document.querySelector('#post-content').focus();
+                // setTimeout(function () {content.focus();}, 1000)
+            });
+        return;
+    } else if (password.length == 0) {
+        Swal.fire({icon: "error", title: "등록실패", text: "비밀번호를 입력해주세요."})
+            .then(() => {
+                document.querySelector('#post-password').focus();
+            });
+        return;
+    }
+
+    // 슈파베이스 스토리지에 저장
+    if (!image_url) {
+        const id = JSON.parse(id).id;
+        var res = await supabase
+            .from('board')
+            .insert([{title, content, author, password, category_id, image_url}])
+            .select();
+    } else {
+        const fileUrl = await uploadFile(image_url);
+        var res = await supabase
+            .from('board')
+            .insert([{title, content, author, password, category_id, image_url: fileUrl}])
+            .select();
+    }
+
+    if (res.status === 201) {
+        Swal.fire({title: "저장성공", icon: "success", confirmButtonText: '확인', draggable: true})
+            .then(() => {
+                noticeSelect();
+            });
+    } else {
+        Swal.fire({title: '저장실패', icon: 'error', confirmButtonText: '확인'});
+    }
+})
+
+// 파일 업로드 url 생성
+async function uploadFile(image_url) {
+    const filename = `${crypto.randomUUID()}.${image_url.name.split('.').pop()}`;
+    await supabase.storage.from('boardimg').upload(filename, image_url);
+
+    const res = await supabase.storage.from('boardimg').getPublicUrl(filename);
+    return res.data.publicUrl;
+}
+
 function boardToggle() {
     document.querySelector('#order-div').classList.toggle('show');
 }
 
+// 게시판 화면에 표시
 async function noticeSelect() {
     const fomatDate = (dateString) => {
         return new Date(dateString).toISOString().split('T')[0];
@@ -13,7 +84,7 @@ async function noticeSelect() {
     let rows = '';
     for (let i = 0; i < res.data.length; i++) {
         rows = rows + `
-            <tr>
+            <tr onclick='postRowClick(this);' style='cursor:pointer;'>
                 <td>${res.data[i].id}</td>
                 <td>${res.data[i].title}</td>
                 <td>${res.data[i].content}</td>
@@ -47,17 +118,35 @@ async function noticeSelect() {
     $boardDiv.classList.add('show');
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+// function postRowClick(trTag) {
+//     const $updateUserId = document.querySelector('#update-user-id');
+//     const $updateName = document.querySelector('#update-name');
+//     const $updateEmail = document.querySelector('#update-email');
+//
+//     const userId = trTag.children[0].innerText;
+//     const userName = trTag.children[1].innerText;
+//     const userEmail = trTag.children[2].innerText;
+//
+//     $updateUserId.innerHTML = userId;
+//     $updateName.value = userName;
+//     $updateEmail.value = userEmail;
+//
+//     const $modal = document.querySelector('#user-modal');
+//     $modal.classList.remove('hidden');
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
     noticeSelect();
 });
 
+// 글쓰기
 function postClick() {
     const $openModal = document.querySelector('#post-modal');
     $openModal.classList.remove('hidden');
 }
 
-function cancelModalClose(){
+// 글쓰기 취소
+function cancelModalClose() {
     const $cancelModal = document.querySelector('#post-modal');
     $cancelModal.classList.add('hidden');
-    console.log('닫았다');
 }
