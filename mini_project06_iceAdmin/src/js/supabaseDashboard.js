871f5e116369a663369b8ccc9f0b72c8bb4db4cd
DESKTOP-29VI7GO\user <sunyoeng423@naver.com>
2025-06-23 10:11:10 +0900

    no message

--- mini_project06_iceAdmin/src/js/supabaseDashboard.js
+++ mini_project06_iceAdmin/src/js/supabaseDashboard.js
@@ -2,6 +2,6 @@ import {supabase} from "./supabase.js";
 
 
 export const getStatesByPeriod = async(year,month,start_date,key)=>{
-    let res = await supabase.rpc("getstatesbyperiod",{year,month,start_date,key});
+    let res = await supabase.rpc("getdashboard",{year,month,start_date,key});
     return res;
 }
